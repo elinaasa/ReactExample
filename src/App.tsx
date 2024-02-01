309d1c24d69b417885a09c7177d19276bf0bc035
Elina Sankila <sankilaelina@gmail.com>
2024-02-01 15:28:34 +0200

    views added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Home from './views/Home'
+import Profile from './views/Profile'
+import Single from './views/Single'
+import Upload from './views/Upload'
 
 const App = () => {
   return (
     <>
       <h1>Media Library</h1>
       <Home />
+      <Profile />
+      <Single />
+      <Upload />
     </>
 
   )
---
+++ src/views/Single.tsx
@@ -0,0 +1,9 @@
+const Single = () => {
+  return (
+    <div>
+      <h2>Single</h2>
+    </div>
+  );
+}
+
+export default Single;
